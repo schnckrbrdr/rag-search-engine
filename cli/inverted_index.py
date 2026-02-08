5c25751ffe8e5433e1ac36d8676f73cf21dd1f2c
schnckrbrdr <schnckrbrdr@gmail.com>
2026-02-08 02:33:43 +0100

    Index search instead of full dictionary search

--- cli/inverted_index.py
+++ cli/inverted_index.py
@@ -1,15 +1,14 @@
 import os
 import pickle
-from utility import load_movies, tokenize
+from utility import load_movies, load_stopwords, tokenize
 
 class InvertedIndex:
 
-    def __init__(self, stopwords: list[str]):
+    def __init__(self) -> None:
         self.index = {}
-        self.docmap = {}
-        self.stopwords = stopwords
+        self.docmap = {}        
 
-    def __add_document(self, doc_id: int, text: str):
+    def __add_document(self, doc_id: int, text: str) -> None:
         tokens = tokenize(text, self.stopwords)
         for token in tokens:            
             if token in self.index:
@@ -26,23 +25,33 @@ class InvertedIndex:
         else:
             return []
 
-    def build(self):
+    def build(self) -> None:
         
         movies = load_movies()
+        stopwords = load_stopwords()
         self.index.clear()
         self.docmap.clear()
         for movie in movies['movies']:
             self.docmap[movie['id']] = movie
             self.__add_document(movie['id'], f"{movie['title']} {movie['description']}")            
 
-    def save(self):
-        if not os.path.isdir(os.path.join(os.path.dirname(os.path.dirname(__file__)), "cache")):
-            os.mkdir(os.path.join(os.path.dirname(os.path.dirname(__file__)), "cache"))
+    def save(self) -> None:
+        try:
+            if not os.path.isdir(os.path.join(os.path.dirname(os.path.dirname(__file__)), "cache")):
+                os.mkdir(os.path.join(os.path.dirname(os.path.dirname(__file__)), "cache"))
 
-        pickle.dump(self.index, open(os.path.join(os.path.dirname(os.path.dirname(__file__)), "cache", "index.pkl"), "wb"))
-        pickle.dump(self.docmap, open(os.path.join(os.path.dirname(os.path.dirname(__file__)), "cache", "docmap.pkl"), "wb"))
+            pickle.dump(self.index, open(os.path.join(os.path.dirname(os.path.dirname(__file__)), "cache", "index.pkl"), "wb"))
+            pickle.dump(self.docmap, open(os.path.join(os.path.dirname(os.path.dirname(__file__)), "cache", "docmap.pkl"), "wb"))
+        except Exception as e:
+            print(f"Error: {e}")
     
-    
+    def load(self) -> bool:
+        try:
+            self.index = pickle.load(open(os.path.join(os.path.dirname(os.path.dirname(__file__)), "cache", "index.pkl"), "rb"))
+            self.docmap = pickle.load(open(os.path.join(os.path.dirname(os.path.dirname(__file__)), "cache", "docmap.pkl"), "rb"))
+            return True
+        except Exception as e:
+            return False
 
     
 
--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -1,56 +1,70 @@
 #!/usr/bin/env python3
 
 import argparse
-from utility import load_movies, load_stopwords, tokenize
+from utility import load_stopwords, tokenize
 from inverted_index import InvertedIndex
 
-def keyword_search(query_string: str, movies_json: list[dict], stopwords: list[str], limit: int = 5) -> list[dict]:
+def keyword_search(query_string: str, inverted_index: InvertedIndex, limit: int = 5) -> list[dict]:
     
     matches = []
-    query_tokens = tokenize(query_string, stopwords)    
-    # Iterate over all movies in list (via dict-entry)
-    for movie in movies_json['movies']:        
-        
-        # tokenize and sanitize movie title
-        movie_tokens = tokenize(movie["title"], stopwords)
-        
-        # set backward counter to end of movie-title tokens
-        i = len(query_tokens) - 1        
-        
-        # iterate over all movie-title tokens
-        while i >= 0:
-            i -= 1
-            
-            # set backward counter to end of query tokens
-            j = len(movie_tokens) - 1
-            
-            # iterate over all query tokens
-            while j >= 0:
-                j -= 1
-                
-                # if a qery token is contained in a movie token, add movie to list of matches and end loop
-                if query_tokens[i] in movie_tokens[j]:
-                    matches.append(movie)
-
-                    # Force end of loop for current movie if match was found                   
-                    i = -1
-                    j = -1
-        
-        # End search when result limit of search is reached
-        if len(matches) >= limit:            
-            break
-
-    sorted_matches = sorted(matches, key=lambda d: d['id'])
-    return sorted_matches
-
-def build_command():
     stopwords = load_stopwords()
-    inverted_index = InvertedIndex(stopwords)
+    
+    if stopwords:
+        query_tokens = tokenize(query_string, stopwords)
+        doc_ids = []
+        for query_token in query_tokens:
+            doc_ids.extend(inverted_index.get_documents(query_token))
+            doc_ids = list(set(doc_ids))
+            if len(doc_ids) >= limit:
+                break
+        matches = []
+        for doc_id in doc_ids:
+            matches.append(inverted_index.docmap[doc_id])            
+        sorted_matches = sorted(matches, key=lambda d: d['id'])
+        return sorted_matches[0:min(len(sorted_matches), limit)]
+    else:
+        return []
+    
+    ##### DEPRECATED #####
+    # Iterate over all movies in list (via dict-entry)
+    #for movie in movies_json['movies']:        
+    #    
+    #    # tokenize and sanitize movie title
+    #    movie_tokens = tokenize(movie["title"], stopwords)
+    #    
+    #    # set backward counter to end of movie-title tokens
+    #    i = len(query_tokens) - 1        
+    #    
+    #    # iterate over all movie-title tokens
+    #    while i >= 0:
+    #        i -= 1
+    #        
+    #        # set backward counter to end of query tokens
+    #        j = len(movie_tokens) - 1
+    #        
+    #        # iterate over all query tokens
+    #        while j >= 0:
+    #            j -= 1
+    #            
+    #            # if a qery token is contained in a movie token, add movie to list of matches and end loop
+    #            if query_tokens[i] in movie_tokens[j]:
+    #                matches.append(movie)
+    #
+    #                # Force end of loop for current movie if match was found                   
+    #                i = -1
+    #                j = -1
+    #    
+    #    # End search when result limit of search is reached
+    #    if len(matches) >= limit:            
+    #        break
+
+    #sorted_matches = sorted(matches, key=lambda d: d['id'])
+    #return sorted_matches
+
+def build_command() -> None:
+    inverted_index = InvertedIndex()
     inverted_index.build()
-    inverted_index.save()
-    docs = inverted_index.get_documents("merida")
-    if len(docs) > 0:
-        print(f"First document for token 'merida' = {docs[0]}")
+    inverted_index.save()    
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Keyword Search CLI")
@@ -65,18 +79,29 @@ def main() -> None:
 
     match args.command:
         case "search":
-            print(f"Searching for: {args.query}")
-            try:  
-                movies_json = load_movies()              
-                stopwords = load_stopwords()
-                if movies_json != None and stopwords != None:
-                    sorted_matches = keyword_search(args.query, movies_json, stopwords)
-                    for i in range(0, min(len(sorted_matches), 6)):
-                        print(f"{i + 1}. {sorted_matches[i]['title']}")
-                else:
-                    print("Error reading data!")
-            except Exception as e:
-                print(f"Error: {e}")
+
+            inverted_index = InvertedIndex()
+            if not inverted_index.load():
+                print("Index not found. Build index first!")
+            else:
+                sorted_matches = keyword_search(args.query, inverted_index)
+                for i in range(0, min(len(sorted_matches), 6)):
+                    print(f"{i + 1}. {sorted_matches[i]['title']}")
+
+            
+        #case "search":
+        #    print(f"Searching for: {args.query}")
+        #    try:  
+        #        movies_json = load_movies()              
+        #        stopwords = load_stopwords()
+        #        if movies_json != None and stopwords != None:
+        #            sorted_matches = keyword_search(args.query, movies_json, stopwords)
+        #            for i in range(0, min(len(sorted_matches), 6)):
+        #                print(f"{i + 1}. {sorted_matches[i]['title']}")
+        #        else:
+        #            print("Error reading data!")
+        #    except Exception as e:
+        #        print(f"Error: {e}")
         case "build":
             try:
                 build_command()
