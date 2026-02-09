125ddeba4864b548690f5cabdc40a9fbd138caa6
schnckrbrdr <schnckrbrdr@gmail.com>
2026-02-09 01:16:55 +0100

    Added TF and IDF

--- cli/inverted_index.py
+++ cli/inverted_index.py
@@ -1,21 +1,28 @@
 import os
 import pickle
 from utility import load_movies, load_stopwords, tokenize
+import collections
+import math
 
 class InvertedIndex:
 
     def __init__(self) -> None:
         self.index = {}
-        self.docmap = {}        
+        self.docmap = {}
+        self.term_frequencies = {}
+        self.stopwords = load_stopwords()   
 
     def __add_document(self, doc_id: int, text: str) -> None:
+        
         tokens = tokenize(text, self.stopwords)
+        self.term_frequencies[doc_id] = collections.Counter()
         for token in tokens:            
             if token in self.index:
                 if doc_id not in self.index[token]:
                     self.index[token].append(doc_id)
             else:
                 self.index[token] = [doc_id]
+            self.term_frequencies[doc_id][token] += 1
         for entry in self.index:
             self.index[entry] = sorted(self.index[entry])            
 
@@ -24,16 +31,43 @@ class InvertedIndex:
             return self.index[term]
         else:
             return []
+        
+    def get_tf(self, doc_id: int, term: str) -> int:
+        if doc_id in self.term_frequencies:
+            tokens = tokenize(term, self.stopwords)
+            if len(tokens) != 1:
+                raise Exception("Error: Too many or no query tokens")
+            else:                
+                return self.term_frequencies[doc_id][tokens[0]] if tokens[0] in self.term_frequencies[doc_id] else 0
+        else:
+            return 0
+
+    def get_idf(self, term: str) -> float:        
+        tokens = tokenize(term, self.stopwords)
+        if len(tokens) != 1:
+            raise Exception("Error: Too many or no query tokens")
+        else:
+            term_match_doc_count = 0
+            total_doc_count = 0
+            for entry in self.docmap:
+                if self.get_tf(entry, term) > 0:
+                    term_match_doc_count += 1
+                total_doc_count += 1
+            
+            return math.log((total_doc_count + 1) / (term_match_doc_count + 1))
+
 
     def build(self) -> None:
-        
         movies = load_movies()
-        stopwords = load_stopwords()
         self.index.clear()
         self.docmap.clear()
+        count = 0
         for movie in movies['movies']:
+            count += 1
             self.docmap[movie['id']] = movie
             self.__add_document(movie['id'], f"{movie['title']} {movie['description']}")            
+            if count % 100 == 0:
+                print(f"{(count * 100)/len(movies['movies'])}%")
 
     def save(self) -> None:
         try:
@@ -42,6 +76,7 @@ class InvertedIndex:
 
             pickle.dump(self.index, open(os.path.join(os.path.dirname(os.path.dirname(__file__)), "cache", "index.pkl"), "wb"))
             pickle.dump(self.docmap, open(os.path.join(os.path.dirname(os.path.dirname(__file__)), "cache", "docmap.pkl"), "wb"))
+            pickle.dump(self.term_frequencies, open(os.path.join(os.path.dirname(os.path.dirname(__file__)), "cache", "term_frequencies.pkl"), "wb"))
         except Exception as e:
             print(f"Error: {e}")
     
@@ -49,6 +84,7 @@ class InvertedIndex:
         try:
             self.index = pickle.load(open(os.path.join(os.path.dirname(os.path.dirname(__file__)), "cache", "index.pkl"), "rb"))
             self.docmap = pickle.load(open(os.path.join(os.path.dirname(os.path.dirname(__file__)), "cache", "docmap.pkl"), "rb"))
+            self.term_frequencies = pickle.load(open(os.path.join(os.path.dirname(os.path.dirname(__file__)), "cache", "term_frequencies.pkl"), "rb"))
             return True
         except Exception as e:
             return False
--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -24,42 +24,6 @@ def keyword_search(query_string: str, inverted_index: InvertedIndex, limit: int 
         return sorted_matches[0:min(len(sorted_matches), limit)]
     else:
         return []
-    
-    ##### DEPRECATED #####
-    # Iterate over all movies in list (via dict-entry)
-    #for movie in movies_json['movies']:        
-    #    
-    #    # tokenize and sanitize movie title
-    #    movie_tokens = tokenize(movie["title"], stopwords)
-    #    
-    #    # set backward counter to end of movie-title tokens
-    #    i = len(query_tokens) - 1        
-    #    
-    #    # iterate over all movie-title tokens
-    #    while i >= 0:
-    #        i -= 1
-    #        
-    #        # set backward counter to end of query tokens
-    #        j = len(movie_tokens) - 1
-    #        
-    #        # iterate over all query tokens
-    #        while j >= 0:
-    #            j -= 1
-    #            
-    #            # if a qery token is contained in a movie token, add movie to list of matches and end loop
-    #            if query_tokens[i] in movie_tokens[j]:
-    #                matches.append(movie)
-    #
-    #                # Force end of loop for current movie if match was found                   
-    #                i = -1
-    #                j = -1
-    #    
-    #    # End search when result limit of search is reached
-    #    if len(matches) >= limit:            
-    #        break
-
-    #sorted_matches = sorted(matches, key=lambda d: d['id'])
-    #return sorted_matches
 
 def build_command() -> None:
     inverted_index = InvertedIndex()
@@ -73,7 +37,14 @@ def main() -> None:
     search_parser = subparsers.add_parser("search", help="Search movies using BM25")
     search_parser.add_argument("query", type=str, help="Search query")
 
-    build_parser = subparsers.add_parser("build", help="(Re)build index of movies")
+    subparsers.add_parser("build", help="(Re)build index of movies")
+
+    term_frequency_parser = subparsers.add_parser("tf", help="Determine occurences of search term in given DocumentID")
+    term_frequency_parser.add_argument("doc_id", type=int, help="DocumentID to search")
+    term_frequency_parser.add_argument("term", type=str, help="Term to determine occurences of")
+
+    inverse_document_frequency_parser = subparsers.add_parser("idf", help="Calculate the inverse document frequency of given term")
+    inverse_document_frequency_parser.add_argument("term", type=str, help="Term to get inverse frequency for")
 
     args = parser.parse_args()
 
@@ -88,20 +59,27 @@ def main() -> None:
                 for i in range(0, min(len(sorted_matches), 6)):
                     print(f"{i + 1}. {sorted_matches[i]['title']}")
 
-            
-        #case "search":
-        #    print(f"Searching for: {args.query}")
-        #    try:  
-        #        movies_json = load_movies()              
-        #        stopwords = load_stopwords()
-        #        if movies_json != None and stopwords != None:
-        #            sorted_matches = keyword_search(args.query, movies_json, stopwords)
-        #            for i in range(0, min(len(sorted_matches), 6)):
-        #                print(f"{i + 1}. {sorted_matches[i]['title']}")
-        #        else:
-        #            print("Error reading data!")
-        #    except Exception as e:
-        #        print(f"Error: {e}")
+        case "tf":
+            inverted_index = InvertedIndex()
+            if not inverted_index.load():
+                print("Index not found. Build index first!")
+            else:
+                try:
+                    print(f"Term frequency of '{args.term}' in document '{args.doc_id}': {inverted_index.get_tf(args.doc_id, args.term)}")                    
+                except Exception as e:
+                    print(f"Error: {e}")
+
+        case "idf":
+            inverted_index = InvertedIndex()
+            if not inverted_index.load():
+                print("Index not found. Build index first!")
+            else:
+                try:
+                    idf = inverted_index.get_idf(args.term)
+                    print(f"Inverse document frequency of '{args.term}': {idf:.2f}")
+                except Exception as e:
+                    print(f"Error: {e}")
+
         case "build":
             try:
                 build_command()
